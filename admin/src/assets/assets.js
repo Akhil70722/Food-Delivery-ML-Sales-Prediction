02a87ce154b3d99098104b91b60e7f452d1fc431
devangit12 <141364835+devangit12@users.noreply.github.com>
2024-11-22 23:59:21 +0530

    Add files via upload

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,13 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
+import customer_icon from './customer_icon.png'
+import feedback_icon from './feedback_icon.png'
+import profit_icon from './profit_icon.png'
+import refund_icon from './refund_icon.png'
+import revenue_icon from './revenue_icon.png'
+import supplier_icon from './supplier_icon.png'
+import analysis_icon from './analysis_icon.png'
 
 export const url = 'http://localhost:4000'
 export const currency = '₹'
@@ -14,6 +21,13 @@ export const assets ={
     order_icon,
     profile_image,
     upload_area,
-    parcel_icon
+    parcel_icon,
+    customer_icon,
+    feedback_icon,
+    profit_icon,
+    refund_icon,
+    supplier_icon,
+    revenue_icon,
+    analysis_icon
 }
 
